c4461c85d808c9457b72669c28cfccdf595bd191
Charles Sneedworth <sneedworth@sevensevenseven.net>
2026-04-16 12:58:54 +0200

    Rework game-ending logic

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
+const MAX_ROUNDS = 5;
+const scoreboard = () => {
+    return `Human: ${humanScore}, CPU: ${computerScore}`;
+};
+
 
 function getComputerChoice() {
     let rng = Math.random();
@@ -18,28 +24,63 @@ buttons.addEventListener('click', (event) => {
 });
 
 function playRound(humanChoice, computerChoice) {
+    rounds++;
+    
     if (humanChoice === computerChoice) {
-        showGameResult("Draw");
+        showRoundResult("draw");
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")) {
         humanScore++;
-        showGameResult("Win");
+        showRoundResult("win");
     } else {
         computerScore++;
-        showGameResult("Lose");
+        showRoundResult("lose");
     }
 }
+
 
 const gameText = document.querySelector(".game-text");
 const scoreText = document.querySelector(".score-text");
 
-function showGameResult(result) {
-    const scoreboard = `Human: ${humanScore}, CPU: ${computerScore}.`;
+
+function showRoundResult(result) {
     console.log(result);
-    gameText.textContent = `You ${result}!`;
-    scoreText.textContent = `${scoreboard}`;
+
+    if (isGameFinished()) {
+        announceGameResult(getGameResult());
+        return;
+    }
+
+    gameText.textContent = `You ${result} round ${(rounds)}!`;
+    scoreText.textContent = `${scoreboard()}`;
 }
 
+function isGameFinished() {
+    if (rounds == MAX_ROUNDS) {
+        return true;
+    } return false;
+}
+
+function getGameResult () {
+        if (humanScore === computerScore) {
+            return "DRAW"
+        } else if (humanScore > computerScore) {
+            return "WIN"
+        }
+        return "LOSE"
+}
+
+function announceGameResult(result) {
+    gameText.textContent = `GAME OVER! YOU ${result}!`;
+    scoreText.textContent = `Final Score: ${scoreboard()}`;
+    resetGame();
+}
+
+function resetGame() {
+    rounds = 0;
+    humanScore = 0;
+    computerScore = 0;
+}
 /* ---------------------------- */
